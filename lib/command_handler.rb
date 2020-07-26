11c4c95900e4a7f6e207daa910b9b5052299b32a
Pedro Fernandes Steimbruch <pedrofsteimbruch@gmail.com>
2020-07-26 18:34:20 -0300

    Implement app command handler

---
+++ lib/command_handler.rb
@@ -0,0 +1,21 @@
+module CommandHandler
+  def with_aggregate(aggregate_class, aggregate_id, &block)
+    aggregate = aggregate_class.new(aggregate_id)
+    stream = stream_name(aggregate_class, aggregate_id)
+    repository.with_aggregate(aggregate, stream, &block)
+  end
+
+  def rehydrate(aggregate, stream)
+    repository.load(aggregate, stream)
+  end
+
+  def stream_name(aggregate_class, aggregate_id)
+    "#{aggregate_class.name}$#{aggregate_id}"
+  end
+
+  protected
+
+  def repository
+    @repository ||= AggregateRoot::Repository.new(Rails.configuration.event_store)
+  end
+end
